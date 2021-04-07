e4c13716e416d72377c5129ff37b5459c4f343c7
youngjamespark <youngjamespark@gmail.com>
2021-04-07 15:18:57 +0900

    test added

--- tests/pesq_test.py
+++ tests/pesq_test.py
@@ -2,6 +2,7 @@ import pypesq
 import os
 
 print("Current working directory = %s", os.getcwd())
+pypesq.evaluate("dml\\1089-134686-0019_ori.pcm", "dml\\1089-134686-0019.pcm", 16000, "wb")
 pypesq.evaluate("1089-134686-0000.wav", "1089-134686-0000.wav", 16000, "wb")
 pypesq.evaluate("voice_8k_le.pcm", "voice_8k_le.pcm", 8000, "nb")
 pypesq.evaluate("voice_8k_le.pcm", "voice_8k_le.711.pcm", 8000, "nb")
