3be3f9b6296f1704eee94ed609cb7b88b871e9aa
youngjamespark <youngjamespark@gmail.com>
2021-04-08 13:54:48 +0900

    to float

--- pesq/pesqmain.h
+++ pesq/pesqmain.h
@@ -1,6 +1,6 @@
 #ifndef __PESQ_MAIN_H__
 #define __PESQ_MAIN_H__
 
-double pesq(char* reference, char* degraded, int rate, char* mode);
+float pesq(char* reference, char* degraded, int rate, char* mode);
 
 #endif  // __PESQ_MAIN_H__
