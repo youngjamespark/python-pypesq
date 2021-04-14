4ccbb83e01082d5d82ae334926ac399804ff2ac9
youngjamespark <youngjamespark@gmail.com>
2021-04-14 14:30:35 +0900

    cpp support.

---
+++ src/pypesq.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <math.h>
+// #include "pesq/pesq.h"
+// #include "pesq/dsp.h"
+#include "..\pesq\pesqmain.h"
+
+#define ITU_RESULTS_FILE          "pesq_results.txt"
+
+double evaluate(char* reference, char* degraded, int sample_rate, char* mode) {
+    double mos;
+    // printf("reference = %s\n", reference);
+    // printf("degraded = %s\n", degraded);
+    // printf("sample rate = %d\n", sample_rate);
+    // printf("mode = %s\n", mode);
+
+    mos = pesq(reference, degraded, sample_rate, mode);
+        
+    return mos;
+}
